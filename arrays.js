4ee972bea12b82ca15b10e624c549446c1cd43a5
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-04-23 04:48:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -46,7 +46,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 //arrays removeElementFromEndOfArray(array) removes the last element from the array:
 function removeElementFromEndOfArray(array){
-return  array.slice(array.length);
+return  array.slice(array.length-1);
 }
 
 //arrays removeElementFromEndOfArray(array) removes the last element from the array
